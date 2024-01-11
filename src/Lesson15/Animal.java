84646aa54fadfdd794134b4562c02280bd87aa1a
Sherzod Khushbakov <sherzodking5752@gmail.com>
2024-01-11 22:36:56 +0500

    enums

---
+++ src/Lesson15/Animal.java
@@ -0,0 +1,33 @@
+package Lesson15;
+
+public enum Animal {
+    CAT(){
+        @Override
+        public void voise() {
+            System.out.println("M-I-Y-A-U");
+        }
+    },DOG(){
+        @Override
+        public void voise() {
+            System.out.println("V-O-V");
+        }
+    },COW(){
+        @Override
+        public void voise() {
+            System.out.println("M-O'-O'-O'");
+        }
+    },HORSE(){
+        @Override
+        public void voise() {
+            System.out.println("X-X-I-I");
+        }
+    },DONKEY(){
+        @Override
+        public void voise() {
+            System.out.println("I-I-A-A");
+        }
+    };
+    public void voise(){
+
+    }
+}
---
+++ src/Lesson15/Main.java
@@ -0,0 +1,17 @@
+package Lesson15;
+
+public class Main {
+    public static void main(String[] args) {
+        Animal animal = Animal.DOG;
+        animal.voise();
+        Animal animal1 = Animal.CAT;
+        animal1.voise();
+        Animal animal2 = Animal.COW;
+        animal2.voise();
+        Animal animal3 = Animal.HORSE;
+        animal3.voise();
+        Animal animal4 = Animal.DONKEY;
+        animal4.voise();
+
+    }
+}
