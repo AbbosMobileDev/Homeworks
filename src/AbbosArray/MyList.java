628ceaf38621aadac8a616d02794ab1004e011ea
Sherzod Khushbakov <sherzodking5752@gmail.com>
2024-02-09 23:51:05 +0500

    My array

--- src/AbbosArray/MyList.java
+++ src/AbbosArray/MyList.java
@@ -1,37 +1,80 @@
 package AbbosArray;
 
-public class MyList<T> {
+import java.util.Arrays;
 
-    private static final Object[] EMPTY_ELEMENTDATA = {};
+public class MyList<T> {
     private int capasity;
     private Object[] obj;
-    private static final Object[] default_obj = {};
-    private int index = 0;
     private int size = 0;
-
     public MyList(int capasity) {
         this.capasity = capasity;
         obj = new Object[capasity];
     }
-
     public MyList() {
         this.capasity = 10;
         obj = new Object[capasity];
     }
 
-    public void add(int element) {
+    public void add(Object c) {
         capasity();
-        obj[index++] = element;
-        size++;
+        Object[] objects = new Object[capasity];
+        int i = 0;
+        for (int j = 0; j < size(); j++) {
+            objects[i] = obj[j];
+            i++;
+        }
+        objects[i] = c;
+        obj = objects;
+        size();
 
     }
     public void capasity() {
-        if (capasity == size) {
+        if (capasity-size<=0) {
             capasity = (capasity * 3) / 2 + 1;
         }
     }
     public T getElement(int indexOfElement){
         return (T) obj[indexOfElement];
     }
+    public int getSize(){
+        return size;
+    }
+    public int getLenght(){
+        return obj.length;
+    }
+    public boolean search(T element){
+        for (Object o : obj) {
+            if (o== element) return true;
+        }
+        return false;
+    }
+    public int size() {
+        int count = 0;
+        for (Object o : obj) {
+            if (o == null) {
+                continue;
+            }
+            count++;
+        }
+        size = count;
+        return size;
+    }
+    public int IndexOfelement(T element){
+        for (int i = 0; i < size()-1; i++) {
+            if (obj[i]==element) return i;
+        }
+        return -1;
+    }
+    public int IndexOfLast(T element){
+        for (int i = size()-1; i>=0; i--) {
+            if (obj[i]==element) return i;
+        }
+        return -1;
+    }
 
+
+    @Override
+    public String toString() {
+        return Arrays.toString(Arrays.copyOf(obj,size));
+    }
 }
--- src/AbbosArray/Poligon.java
+++ src/AbbosArray/Poligon.java
@@ -2,11 +2,27 @@ package AbbosArray;
 
 public class Poligon {
     public static void main(String[] args) {
-        MyList<Integer> abbosList = new MyList<>(12);
-
+        MyList<Integer> abbosList = new MyList<>();
         abbosList.add(21);
         abbosList.add(212);
+        abbosList.add(34);
+        abbosList.add(34);
+        abbosList.add(34);
+        abbosList.add(34);
+        abbosList.add(34);
+        abbosList.add(34);
+        abbosList.add(34);
+        abbosList.add(34);
+        abbosList.add(34);
+        abbosList.add(34);
+        abbosList.add(34);
+        abbosList.add(34);
 
-        System.out.println();
+
+
+
+        System.out.println(abbosList);
+
+        System.out.println(abbosList.getSize());
     }
 }
